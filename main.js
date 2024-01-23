5a94c5e47c90d8de6039e2601461818d0f7cfb1e
Harshil-0707 <chaurasiyaharshil7705@gmail.com>
2024-01-23 23:59:01 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const company = document.getElementById('company');
+const features = document.getElementById('features');
+const featureOps = document.getElementById('featureOps');
+const companyOps = document.getElementById('companyOps');
+
+
+features.onclick = () => {
+    featureOps.style.display = "block";
+};
