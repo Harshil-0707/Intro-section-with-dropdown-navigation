9c5c5599699c2b0183db1822031ba429f9c6296a
Harshil-0707 <chaurasiyaharshil7705@gmail.com>
2024-01-27 22:39:16 +0530

    initial commit

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
-const link = document.querySelectorAll(".link");
+const closeMenu = document.getElementById("close-menu");
+const linksRegister = document.getElementById("links-register1");
+const openMenu = document.getElementById("open-menu");
 const company = document.getElementById("company");
 const features = document.getElementById("features");
 
 features.addEventListener("click", () => {
-  features.nextElementSibling.classList.toggle("hide");
   company.nextElementSibling.classList.add("hide");
+  features.nextElementSibling.classList.toggle("hide");
 });
 
 company.addEventListener("click", () => {
   company.nextElementSibling.classList.toggle("hide");
   features.nextElementSibling.classList.add("hide");
 });
+
+openMenu.onclick = () => {
+  openMenu.style.display = "none";
+  closeMenu.style.display = "block";
+  linksRegister.style.display = "block";
+};
+
+closeMenu.onclick = () => {
+  closeMenu.style.display = "none";
+  openMenu.style.display = "block";
+  linksRegister.style.display = "none";
+};
