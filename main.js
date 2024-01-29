fa60388fb18c369bcbcd626e1a782bb08e9a34f2
Harshil-0707 <chaurasiyaharshil7705@gmail.com>
2024-01-29 21:08:15 +0530

    Added final logic and some style

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const linksRegister = document.getElementById("links-register1");
-const openMenu = document.getElementById("open-menu");
 const company = document.querySelectorAll(".two");
 const features = document.querySelectorAll(".one");
-const icons = document.querySelectorAll(".arrowImg img");
+const openMenu = document.getElementById("open-menu");
+const linksRegister = document.getElementById("links-register1");
+// const on = document.getElementsByTagName
 
 features.forEach((fun) => openDropDown(fun));
 company.forEach((com) => openDropDown(com));
@@ -10,6 +10,13 @@ company.forEach((com) => openDropDown(com));
 function openDropDown(args) {
   args.addEventListener("click", () => {
     args.nextElementSibling.classList.toggle("hide");
+    const icon = args.querySelector(".arrowImg img");
+    icon.classList.toggle("arrow");
+    if (icon.classList.contains("arrow")) {
+      icon.src = "./images/icon-arrow-up.svg";
+    } else {
+      icon.src = "./images/icon-arrow-down.svg";
+    }
   });
 }
 
